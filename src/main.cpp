bacb6d12654bf74f2cf03aa4e81ae6c207f89d26
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-19 14:39:20 -0300

    feat: initialize global variables and prepare main `setup` and `loop` functions

--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,109 @@
+#include "arduino_impls.hpp"
+#include "arduino_utils.hpp"
+#include "display.hpp"
+#include "rotary_encoder.hpp"
+#include "simple_timer.hpp"
 #include <Arduino.h>
+
+using tarefa3::arduino::ArduinoRotaryEncoderPinManager;
+using tarefa3::arduino::ArduinoSevenSegmentsDisplayDriver;
+using tarefa3::arduino::is_debounced;
+using tarefa3::core::DisplayDigit;
+using tarefa3::rotary_encoder::RotaryEncoder;
+using tarefa3::timer::SimpleTimer;
+
+// pinos do rotary encoder
+#define SWITCH PB2
+#define OUTPUT_A PB1
+#define OUTPUT_B PB0
+
+// pinos do display
+#define DIG1 A3
+#define DIG2 A4
+#define DIG3 A5
+#define DIG4 A2
 
 void setup()
 {
+  // reforçando:
+  // 1 = output
+  // 0 = input
+  DDRB = 0;    // todos os pinos da seção B são entradas (inputs)
+  DDRD = 0xFF; // todos os pinos na seção D são saídas (outputs) — para os
+               // segmentos do display
+  DDRC = 0x0F; // os 4 primeiros bits da seção C são saídas (outputs) — para os
+               // dígitos do display
+  DDRB &= ~(1 << SWITCH); // torna o pino que recebe o SWITCH do rotary encoder
+                          // em uma entrada
+  PORTB |= (1 << SWITCH); // habilita o resistor de pull-up interno da entrada
+                          // do switch
 }
+
+// timers pra usar nos debounces
+volatile static auto rotation_timer = millis();
+
+// inicialização das variáveis globais necessárias para manipular os
+// componentes no simulador
+static const auto arduino_pin_manager = ArduinoRotaryEncoderPinManager();
+static const auto arduino_display_driver = ArduinoSevenSegmentsDisplayDriver(
+    &PORTC, &PORTD, PD0, PB1, PB2, PB3, PB4, PB5, PB6);
+static auto rotary_encoder =
+    RotaryEncoder(OUTPUT_A, OUTPUT_B, SWITCH, &arduino_pin_manager);
+static auto timer_display =
+    SimpleTimer(DIG1, DIG2, DIG3, DIG4, &arduino_display_driver);
+
+void handle_multiplex_timer_display(
+    const ArduinoSevenSegmentsDisplayDriver *driver, SimpleTimer *timer);
 
 void loop()
 {
+  const auto rotary_encoder_report = rotary_encoder.get_report();
+
+  if (rotary_encoder_report.has_rotated && is_debounced(&rotation_timer))
+  {
+    if (rotary_encoder_report.has_rotated_clockwise)
+    {
+      timer_display.increment_seconds();
+    }
+    else
+    {
+      timer_display.decrement_seconds();
+    }
+  }
+
+  handle_multiplex_timer_display(&arduino_display_driver, &timer_display);
+}
+
+void handle_multiplex_timer_display(
+    const ArduinoSevenSegmentsDisplayDriver *driver, SimpleTimer *timer)
+{
+  static unsigned int display_multiplex_timer = millis();
+  static uint8_t digit = 1;
+
+  driver->turn_all_off();
+
+  if (millis() - display_multiplex_timer >= 5)
+  {
+    switch (digit)
+    {
+    case 1:
+      timer->update_display(SimpleTimer::TimeFragment::MinutesTen);
+      break;
+
+    case 2:
+      timer->update_display(SimpleTimer::TimeFragment::MinutesUnit);
+      break;
+
+    case 3:
+      timer->update_display(SimpleTimer::TimeFragment::SecondsTen);
+      break;
+
+    case 4:
+      timer->update_display(SimpleTimer::TimeFragment::SecondsUnit);
+      break;
+    }
+
+    digit = digit == 4 ? 1 : digit + 1;
+    display_multiplex_timer = millis();
+  }
 }

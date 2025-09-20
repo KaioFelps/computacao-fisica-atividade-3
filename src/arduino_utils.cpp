24b632618cdfc0189e19a67a9ae4a2536c5eabc7
Kaio Felps <kaiofelipe2005@gmail.com>
2025-09-19 22:20:25 -0300

    feat: decrease delay resistance from 125ms to 25ms

--- src/arduino_utils.cpp
+++ src/arduino_utils.cpp
@@ -5,7 +5,7 @@ namespace tarefa3::arduino
 {
 bool is_debounced(volatile unsigned long *debounce)
 {
-  const size_t delay_resistance = 125;
+  const size_t delay_resistance = 25;
 
   if ((millis() - *debounce) <= delay_resistance)
   {
